56f2ae2c8f33c9b0b560bdd041843e1979e7ac8d
hokudai <ken4600223@gmail.com>
2021-09-16 10:27:58 +0900

    add_change_20210916

--- sample.py
+++ sample.py
@@ -59,9 +59,9 @@ def main():
 
     kappa=0.4
     sgm_c=1.3
-    n=5
+    n=5000
     Ri=0.01
-    ds=1.0/(1.0*10**3)
+    ds=1.0/(1.0*10**4)
     ks=2.5*ds
     zb=1.0*ds
     ub=1.0/kappa*math.log(30.0*zb/ks)
@@ -83,71 +83,71 @@ def main():
             uo[1:n]=u[1:n]
             co[1:n]=c[1:n]
 
-        #------------  gradient	-------------
-        dudzm[0]=2.0*(uo[1]-ub)/dz
-        dudzm[1:n-1]=(uo[1:n-1]-uo[1:n-1])/dz
-        dudzm[n]=0.0
-        dudz[1:n]=(dudzm[0:n-1]+dudzm[1:n])/2.0
+            #------------  gradient	-------------
+            print('Initial velocity')
+            dudzm[0]=2.0*(uo[1]-ub)/dz
+            dudzm[1:n-1]=(uo[1:n-1]-uo[1:n-1])/dz
+            dudzm[n]=0.0
+            dudz[1:n]=(dudzm[0:n-1]+dudzm[1:n])/2.0
 
-        dcdzm[0]=2.0*(co[1]-cb)/dz
-        dcdzm[1:n-1]=(co[2:n]-co[1:n-1])/dz
-        dcdzm[n]=2.0*(0-co[n])/dz
-        
-        dcdz[1:n]=(dcdzm[0:n-1]+dcdzm[1:n])/2.0
-        print('Initial velocity')
+            dcdzm[0]=2.0*(co[1]-cb)/dz
+            dcdzm[1:n-1]=(co[2:n]-co[1:n-1])/dz
+            dcdzm[n]=2.0*(0-co[n])/dz
+            
+            dcdz[1:n]=(dcdzm[0:n-1]+dcdzm[1:n])/2.0
+            
+            #-------	eddy viscosity	----------------
+            print('eddy viscosity')
+            cf[0:n]=(dudzm[0:n]**2-1.350*Ri*dcdzm[0:n])/(dudzm[0:n]**2-14.85*Ri*dcdzm[0:n])
+            nu_tm[0:n]=kappa**2*zm[0:n]**2*dudzm[0:n]*cf[0:n]
+            nu_t[1:n]=(nu_tm[0:n-1]+nu_tm[1:n])/2.0
+            nu_tcm[0:n]=nu_tm[0:n]/sgm_c
+            
+            #------- 	velocity	----------------------
+            print('velocity')
+            D_u[0:n]=nu_tm[0:n]/dz
 
-		#-------	eddy viscosity	----------------
-		cf[0:n]=(dudzm[0:n]**2-1.350*Ri*dcdzm[0:n])/(dudzm[0:n]**2-14.85*Ri*dcdzm[0:n])
-        nu_tm[0:n]=kappa**2*zm[0:n]**2*dudzm[0:n]*cf[0:n]
-		nu_t[1:n]=(nu_tm[0:n-1]+nu_tm[1:n])/2.0
-		nu_tcm[0:n]=nu_tm[0:n]/sgm_c
-        print('eddy viscosity')
+            a_u[1]=0.0
+            a_u[2:n]=-D_u[1:n-1]
+            b_u[1]=dz/dt+2.0*D_u[0]+D_u[1]
+            b_u[2:n-1]=dz/dt+D_u[1:n-2]+D_u[2:n-1]
+            b_u[n]=dz/dt+D_u[n-1]
+            c_u[1:n-1]=-D_u[1:n-1]
+            c_u[n]=0.0
 
-		#------- 	velocity	----------------------
+            r_u[1]=(uo[1]/dt+c[1])*dz+2.0*D_u[0]*ub
+            r_u[2:n]=(uo[2:n]/dt+c[2:n])*dz
 
-		D_u[0:n]=nu_tm[0:n]/dz
+            tridag(a_u,b_u,c_u,r_u,u,n)
 
-		a_u[1]=0.0
-		a_u[2:n]=-D_u[1:n-1]
-		b_u[1]=dz/dt+2.0*D_u[0]+D_u[1]
-		b_u[2:n-1]=dz/dt+D_u[1:n-2]+D_u[2:n-1]
-		b_u[n]=dz/dt+D_u[n-1]
-		c_u[1:n-1]=-D_u[1:n-1]
-		c_u[n]=0.0
+            #-------------- concentration --------------------------
+            print('concentration')
+            zeta[0]=0
+            for i in range(1,n):
+                zeta[i]=zeta[i-1]+vs/nu_t[i]*dz
+                # write(*,*) i, zeta(i), nu_t(i)
 
-		r_u[1]=(uo[1]/dt+c[1])*dz+2.0*D_u[0]*ub
-		r_u[2:n]=(uo[2:n]/dt+c[2:n])*dz
+            fum[0:n]=math.exp(-zeta[0:n])
 
-		tridag(a_u,b_u,c_u,r_u,u,n)
+            Xm(0)=0.0
+            for i in range(1,n):
+                Xm[i]=Xm[i-1]+(fum[i-1]+fum[i])*dz/2.0
+                # write(*,*) i, fum(i)
+            
 
-		#-------------- concentration --------------------------
+            cb=1/Xm[n]
+            cm[0:n]=cb*fum[0:n]
+            c[1:n]=(cm[0:n-1]+cm[1:n])/2.0
 
-		zeta[0]=0
-		for i in range(1,n):
-			zeta[i]=zeta[i-1]+vs/nu_t[i]*dz
-			# write(*,*) i, zeta(i), nu_t(i)
+            #---------  error	-----------------
+            print('error')
+            err_u[1:n]=math.fabs(uo[1:n]-u[1:n])
+            err_c[1:n]=math.fabs(co[1:n]-c[1:n])
+            maxerr=max(max(err_u[1:n]),max(err_c[1:n]))
 
-		fum[0:n]=math.exp(-zeta[0:n])
+            # write(*,*) maxerr, cb
 
-		Xm(0)=0.0
-		for i in range(1,n):
-			Xm[i]=Xm[i-1]+(fum[i-1]+fum[i])*dz/2.0
-			# write(*,*) i, fum(i)
-		
-
-		cb=1/Xm[n]
-		cm[0:n]=cb*fum[0:n]
-		c[1:n]=(cm[0:n-1]+cm[1:n])/2.0
-
-		#---------  error	-----------------
-
-		err_u[1:n]=math.fabs(uo[1:n]-u[1:n])
-		err_c[1:n]=math.fabs(co[1:n]-c[1:n])
-		maxerr=max(max(err_u[1:n]),max(err_c[1:n]))
-
-        # write(*,*) maxerr, cb
-
-        # write(30,'(2f15.5)') vs, cm(0)
+            # write(30,'(2f15.5)') vs, cm(0)
 
 
 if __name__ == '__main__':
