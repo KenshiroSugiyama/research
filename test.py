e5db0271685e29f539782778cc1da9765ecc4409
hokudai <ken4600223@gmail.com>
2022-01-26 20:15:00 +0900

    for_E_vs

--- test.py
+++ test.py
@@ -5,43 +5,35 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker
 
 def main():
-    Rps = []
-    Rfs = []
-    for i in range(1,101):
-        Rp = i
-        a1=2.891394478
-        a2=0.95296
-        a3=0.056834671
-        a4=0.0002892046
-        a5=0.000244647
+    S=0.1
+    Fr=np.sqrt(S)
+    # Fr=S**2
+    Rp_s=[1,10,100]
+    for index, Rp in enumerate(Rp_s):
+        print(Rp)
+        E_all=[]
+        vs_all=[]
+        for k in range(1,101):
+            vs=0.01*k	
+            # print(vs)
+            vs_all.append(vs)
+            E=(5.73*10**(-3))*((1.0/vs)**1.31)*(Fr**1.59)*(Rp**(-0.86))
+            E_all.append(E)
+            # print(E)
+        
+        
+        plt.xlabel('vs')
+        plt.ylabel('E')
+        # plt.xlim(0.01,1)
+        # plt.ylim(0.0001,1)
+        plt.xscale('log')
+        plt.yscale('log')
+        color=["b:","g:","r:","c","y:","k:"]
+        label=["Rp=1","Rp=10","Rp=100"]
+        plt.plot(vs_all,E_all,color[index], label=label[index])
+        plt.legend()
+    plt.show()
 
-        Rf=np.exp(-a1+a2*np.log(Rp)-a3*(np.log(Rp))**2.0+a4*(np.log(Rp))**3.0-a5*(np.log(Rp))**4.0) #Dietrich falling velocity
-        
-        # print(Rp, end=' ')
-        # print(Rf)
-        Rps.append(Rp)
-        print(Rps)
-        Rfs.append(Rf)
-        #グラフ出力
-        # plt.title('E-vs')
-        
-        # color=["b:","g:","r:","c","y:","k:"]
-    
-    
-    plt.xlabel('Rp')
-    plt.ylabel('Rf')
-    plt.xscale("log")
-    # plt.yscale("log")
-    plt.xlim(0,100)
-    # pos = [1, 10, 100] 
-    # ticks = ['1', '10']
-    # plt.set_xticks(pos)
-    # plt.set_xticklabels(ticks)
-    plt.ylim(0,1.5)
-    plt.plot(Rps,Rfs,"k:", linestyle = 'solid')
-    # plt.legend()
-    
-    plt.show() # プロットを表示
     
 
 if __name__ == "__main__":
